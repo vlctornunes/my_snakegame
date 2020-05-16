a75e2f330493b0d024daf8cad7beddcef848109f
Victor <vn28007@gmail.com>
2020-05-16 15:42:04 -0300

    Comments added; now the snake eats the apple (and gets bigger).

--- snake.py
+++ snake.py
@@ -1,6 +1,17 @@
 import pygame, random
 from pygame.locals import *
 
+def on_grid_random():
+    x = random.randint(0,290)
+    y = random.randint(0,290)
+    #fazer uma divisão inteira por 10 e multiplicar por 10
+    return (x//10 * 10, y//10 * 10)
+    #gera uma localização mutipla de 10 pra botar a maçã
+
+def collision(c1, c2):
+    return (c1[0] == c2[0] and c1[1] == c2[1])
+
+
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -10,16 +21,20 @@ pygame.init()
 screen = pygame.display.set_mode((300, 300))
 pygame.display.set_caption('Snake by Victor N.')
 
+#o corpo da cobra é uma tupla (começa com 3 endereços, seções)
 snake = [(200, 200), (210, 200), (220, 200)]
+#definição do que é formado o corpo da cobra
 snake_skin = pygame.Surface((10,10))
 snake_skin.fill((255,255,255))
 
+#posição aletória pra maçã
+apple_pos = on_grid_random()
 apple = pygame.Surface((10,10))
 apple.fill((255,0,0))
-apple_pos = (random.randint(0,290), random.randint(0,290))
 
 my_direction = LEFT
 
+#controle do FPS
 clock = pygame.time.Clock()
 
 while True:
@@ -38,6 +53,14 @@ while True:
         if event.key == K_LEFT:
             my_direction = LEFT
 
+    #se há a colisão, a maçã vai pra cauda da cobra
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
+
+    for i in range(len(snake) -1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if my_direction == RIGHT:
@@ -47,12 +70,10 @@ while True:
     if my_direction == LEFT:
         snake[0] = (snake[0][0] - 10, snake[0][1])
 
-    for i in range(len(snake) -1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
-
     screen.fill((0,0,0))
     screen.blit(apple, apple_pos)
     for pos in snake:
         screen.blit(snake_skin,pos)
 
+    #FPS
     pygame.display.update()
